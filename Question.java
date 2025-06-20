c04c837c7403186d6a9f979feb657f382ca91990
Ndivholious112 <nemutshilindivhoconfidence@gmail.com>
2025-06-20 10:33:18 +0200

    Question class done

--- Question.java
+++ Question.java
@@ -1,2 +1,21 @@
 public class Question {
+    private String question;
+    private char correctAnswer;
+    private String option[];
+
+    public Question(String question, char correctAnswer, String[] option) {
+        this.question = question;
+        this.correctAnswer = correctAnswer;
+        this.option = option;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+    public char getCorrectAnswer() {
+        return correctAnswer;
+    }
+    public String[] getOption() {
+        return option;
+    }
 }
